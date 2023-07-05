f1ec2da926dcea12f890ba8c64c549b5566396b4
vaibhav <vaibhavsahni2001@gmail.com>
2023-07-05 19:52:34 +0530

    initial2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import Food from "../backend/DB/Food.js";
+import Food from "./DB/Food.js";
 const app = express();
 
 const corsConfig = {
